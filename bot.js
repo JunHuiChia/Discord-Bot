890f5211e9d373631f26bcce5fdb7567102352ab
Jun Hui Chia <up937100@myport.ac.uk>
2020-12-16 21:55:21 +0000

    editted output msg

--- bot.js
+++ bot.js
@@ -320,10 +320,10 @@ client.on('message', (message) => {
 	if (!message.content.startsWith(BOT_PREFIX) || message.author.bot) return;
 	const args = message.content.slice(BOT_PREFIX.length).split(' ');
 	const command = args.shift().toLowerCase();
-	console.log('args' + args);
-	console.log('args' + command);
 	if (command === 'w' || command === 'weather') {
-		message.reply('It is fucking sunny and 10°C in ' + args);
+		message.reply(
+			`It is fucking sunny and 10°C in \`${args.shift.toUpperCase()}\``
+		);
 	} else {
 		console.log('dont work');
 	}
