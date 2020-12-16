3679ceeca5b366eda18e3995ea8e5eaab1af18d3
Jun Hui Chia <up937100@myport.ac.uk>
2020-12-16 21:34:31 +0000

    forgot a )...

--- bot.js
+++ bot.js
@@ -316,15 +316,14 @@ client.on('message', (msg) => {
 	}
 });
 
-client.on('message', message => {
-    if (!message.content.startsWith(BOT_PREFIX) || message.author.bot) return;
-    const args = message.content.slice(prefix.length).split(' ');
-    const command = args.shift().toLowerCase();
-    console.log(args + command);
-    if (command === 'w' || "weather"){
-        message.reply("It is fucking sunny and 10°C in " + args);
-    }
-
-}
+client.on('message', (message) => {
+	if (!message.content.startsWith(BOT_PREFIX) || message.author.bot) return;
+	const args = message.content.slice(prefix.length).split(' ');
+	const command = args.shift().toLowerCase();
+	console.log(args + command);
+	if (command === 'w' || 'weather') {
+		message.reply('It is fucking sunny and 10°C in ' + args);
+	}
+});
 
 client.login(process.env.BOT_TOKEN);
