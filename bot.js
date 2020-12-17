71fc1d2a3c44d4dfc04158841b6d15d691328e91
Jun Hui Chia <up937100@myport.ac.uk>
2020-12-17 15:23:12 +0000

    error handling didnt work

--- bot.js
+++ bot.js
@@ -326,10 +326,9 @@ client.on('message', (message) => {
 	if (command === 'w' || command === 'weather') {
 		fetch(
 			`https://api.openweathermap.org/data/2.5/weather?q=${args}&units=metric&appid=${process.env.W_API_KEY}`
-		).then(function (response) {
-			response
-				.json()
-				.then(function (weather_data) {
+		)
+			.then(function (response) {
+				response.json().then(function (weather_data) {
 					let apiData = weather_data;
 					let currentTemp = apiData.main.temp;
 					let maxTemp = apiData.main.temp_max;
@@ -364,11 +363,11 @@ client.on('message', (message) => {
 						.then(
 							msg.member.lastMessage.delete({ timeout: 600000 })
 						);
-				})
-				.catch((err) => {
-					message.reply(`Enter a vailid city name`);
 				});
-		});
+			})
+			.catch((err) => {
+				message.reply(`Enter a vailid city name`);
+			});
 	}
 });
 
